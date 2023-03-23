3f4eee38ee2b4bce5c6e6c33e20166cc82bad975
BeJuanitox <jsebastian23@hotmail.com>
2023-03-23 14:36:01 -0500

    Delete common module

--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
@@ -8,9 +7,6 @@ import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
   exports: [
     MatToolbarModule,
     MatIconModule,
