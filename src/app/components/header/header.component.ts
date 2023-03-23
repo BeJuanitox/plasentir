1e62a270a624dc764ab416d8acaf14240051de81
Andres2D <andres2d1997.1206@gmail.com>
2023-03-22 22:34:06 -0500

    Implement shopping cart subscriptions

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  shoppingCartCounter: number = 0;
 
+  constructor(private readonly shoppingCartService: ShoppingCartService) { }
+  
+  ngOnInit(): void {
+    this.shoppingCartService.subject$.subscribe(() => {
+      this.shoppingCartCounter = this.shoppingCartService.shoppingCart.length;
+    });   
+  }
 }
