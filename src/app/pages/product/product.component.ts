a44253f04540c8ba4f2ef3a107f9d98dfdb02c22
Andres2D <andres2d1997.1206@gmail.com>
2023-04-03 23:22:41 -0500

    Add product detail page

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../interfaces/product.interface';
+import { ProductService } from '../../services/product.service';
+import { FirebaseService } from '../../services/firebase.service';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-product',
@@ -10,11 +13,35 @@ import { Product } from '../../interfaces/product.interface';
 export class ProductComponent implements OnInit {
 
   product?: Product;
+  productImage: string = '';
 
-  constructor(private readonly route: ActivatedRoute) { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly productService: ProductService,
+    private readonly firebaseService: FirebaseService,
+    private readonly shoppingCartService: ShoppingCartService,
+  ) { }
 
   ngOnInit(): void {
-    console.log(this.route.snapshot.params['id']);
+    const productCode = this.route.snapshot.params['id'];
+    this.productService.getProduct(productCode).subscribe(product => {
+      this.product = product;
+      this.getFirebaseUrlImage(product.image);
+    });
   }
 
+  getFirebaseUrlImage(image: string) {
+    this.firebaseService.getStoredImage(image)
+    .then(url => {
+      this.productImage = url;
+    });
+  }
+
+  get isOnCart(): boolean {
+    return this.shoppingCartService.isOnCart(this.product?.code || '');
+  }
+
+  addRemoveToCart(): void {
+    this.shoppingCartService.addRemoveToCart(this.product!);
+  }
 }
